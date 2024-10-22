db4e270fb5fdbfc958027ceced3600fec4727590
[Aakarsh Sharma] <[aakarshjai@gmail.com]>
2024-10-22 15:22:02 +0530

    editing navbar

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,7 +1,11 @@
-
+import Navbar from "./Components/Navbar.jsx"
 
 function App() {
-  
+  return (
+    <>
+    <Navbar/>
+    </>
+  );
 }
 
 export default App
---
+++ project2/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+        <div className="logo">
+            <img src="/images/logo.png" alt="logo" />
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
