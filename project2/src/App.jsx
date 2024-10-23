406630e9e88776dba0358c78317ee3ed45b9b0cb
Aakarsh Sharma <aakarshjai@gmail.com>
2024-10-23 15:23:38 +0530

    imported fonts

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./Components/Navigation/Navigation.jsx"
 import ContactHeader from "./Components/ContactHeader/ContactHeader.jsx";
+import ContactForm from "./Components/ContactForm/ContactForm.jsx";
 
 function App() {
   return (
     <>
     <Navigation/>
     <ContactHeader/>
+    <ContactForm/>
     </>
   );
 }
---
+++ project2/src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,25 @@
+import Button from '../Button/Button';
+import styles from './ContactForm.module.css';
+import { MdMessage } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+const ContactForm = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.contact_form}>
+            <div className={styles.top_btn}>
+            <Button text="VIA SUPPORT CHAT" icon= {<MdMessage fontSize="24px"/>}/>
+            <Button text="VIA CALL" icon= {<FaPhoneAlt fontSize="24px"/>}/>
+            </div>
+            <Button 
+            isOutline = {true}
+            text="VIA EMAIL FORM" icon= {<MdEmail fontSize="24px"/>}/>
+            
+        </div>
+        <div className={styles.contact_image}></div>
+    </section>
+
+  )
+}
+
+export default ContactForm
