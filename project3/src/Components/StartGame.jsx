425f3f8402956a0ead00829454322988c4a57187
Aakarsh Sharma <aakarshjai@gmail.com>
2024-11-01 17:32:05 +0530

    desing front page

--- project3/src/Components/StartGame.jsx
+++ project3/src/Components/StartGame.jsx
@@ -1,7 +1,25 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    max-width: 1180px;
+`;
+
+const Button = styled.button`
+    background-color: black;
+    color: white;
+    padding: 10px;
+`
 
 const StartGame = () => {
   return (
-    <div>StartGame</div>
+    <Container>
+        <img src="/images/dices.png" alt="dices"/>
+        <div>
+            <h1>DICE GAME</h1>
+            <Button>Play Now</Button>
+        </div>
+    </Container>
+    
   )
 }
 
