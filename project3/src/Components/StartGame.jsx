7a4a0036bae04948da4917d3f1eb0deb17a54db5
Aakarsh Sharma <aakarshjai@gmail.com>
2024-11-01 22:40:10 +0530

    front page final

--- project3/src/Components/StartGame.jsx
+++ project3/src/Components/StartGame.jsx
@@ -1,20 +1,12 @@
 import styled from "styled-components";
 
-const Container = styled.div`
-    max-width: 1180px;
-`;
-
-const Button = styled.button`
-    background-color: black;
-    color: white;
-    padding: 10px;
-`
-
 const StartGame = () => {
   return (
     <Container>
-        <img src="/images/dices.png" alt="dices"/>
         <div>
+            <img src="/images/dices.png" alt="dices"/>
+        </div>
+        <div className="content">
             <h1>DICE GAME</h1>
             <Button>Play Now</Button>
         </div>
@@ -24,3 +16,39 @@ const StartGame = () => {
 }
 
 export default StartGame
+
+const Container = styled.div`
+    max-width: 1180px;
+    display: flex;
+    margin: 0 auto;
+    height: 100vh;
+    align-items: center;
+
+    .content h1{
+        font-size: 96px;
+        white-space: nowrap;
+    }
+    
+`;
+
+const Button = styled.button`
+    background-color: black;
+    color: white;
+    padding: 10px 18px;
+    border-radius: 5px;
+    cursor: pointer;
+    min-width: 220px;
+    border: none;
+    font-size: 16px;
+    border: 1px solid transparent;
+    transition: 0.4s background ease-in;
+
+    &:hover{
+        background-color: white;
+        border: 1px solid black;
+        color: black;
+        transition: 0.3s background ease-in;
+    }
+
+
+`
